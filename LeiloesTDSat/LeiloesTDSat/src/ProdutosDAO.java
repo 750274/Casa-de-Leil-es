fd3a872a592401e4402d039ed77df8ceec430259
Joao Vitor Teixeira <Jvtgoulart@gmail.com>
2025-03-04 10:53:24 -0300

    Botao de salvar cadastrando produto

--- LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
@@ -25,7 +26,26 @@ public class ProdutosDAO {
     public void cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
+    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        
+        prep.executeUpdate();
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro.getMessage());
+    } finally {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
         
         
     }
